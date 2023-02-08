69af0ed94829eb7f9da18ef4456fcf948e2477ff
SaqibAjmain <46943721+SaqibAjmain@users.noreply.github.com>
2023-02-07 22:06:00 -0500

    started navbar html document and js
    This will probably be used in every page, I will just do some testing with css and page styling.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,1 @@
-<<<<<<< HEAD
-fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata")
-.then(function(response){
-    return response.json()})
-    .then(function(data){
-        console.log(data);
-    });
-=======
 // digitalcookbook-5702476ac48c92cd07ae803948186878208195001536311250
->>>>>>> 503e1e050e9c05571c7b88bfae96573eb796f501
