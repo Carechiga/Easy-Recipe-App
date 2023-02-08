b32cbae845cfe81aec68cc13dc10009f62d4c61e
Chris <carechiga43@gmail.com>
2023-02-08 09:57:31 -0800

    created basic structure to display recipe ingredients and instructions when the recipe-page.html is opened. Pulls recipe ID from local storage

--- assets/js/recipe-script.js
+++ assets/js/recipe-script.js
@@ -2,27 +2,35 @@ var mealID = localStorage.getItem('mealID');
 var recipeName = document.getElementById('recipe-name');
 var recipeImg = document.getElementById('recipe-img');
 var ingredientList = document.getElementById('ingredient-list');
-var ingredientArray = ['strIngredient1', 'strIngredient2', 'strIngredient3', 'strIngredient4','strIngredient5', 'strIngredient6', 'strIngredient7', 'strIngredient8', 'strIngredient9', 'strIngredient10', 'strIngredient11', 'strIngredient12','strIngredient13', 'strIngredient14', 'strIngredient15', 'strIngredient16','strIngredient17', 'strIngredient18', 'strIngredient19', 'strIngredient20',];
+var ingredientItem = document.querySelectorAll('.ingredient-item');
+var instructions = document.getElementById('instructions');
 
+
+//loads recipe info using mealID stored in localStorage
 function recipeDisplay(){
     fetch("https://www.themealdb.com/api/json/v2/9973533/lookup.php?i=" + mealID)
 .then(function(response){
     return response.json()})
     .then(function(data){
         console.log(data);
-        recipeName.textContent = data.meals[0].strMeal;
-        recipeImg.src = data.meals[0].strMealThumb;
-
-    for(var i = 0; i < ingredientArray.length; i++){
-        var currentIngredient = ingredientArray[i];
-        if(data.meals[0].currentIngredient !== ""){
-            console.log(currentIngredient);
-            var recipeListItem = document.createElement('li');
-            recipeListItem.textContent = data.meals[0].currentIngredient;
-            recipeListItem.append(ingredientList);
+        var selectedMeal = data.meals[0];
+        recipeName.textContent = selectedMeal.strMeal;
+        recipeImg.src = selectedMeal.strMealThumb;
+        var instructionsString = selectedMeal.strInstructions;
+        var instructionsArray = instructionsString.split('.');
+                
+        for(var i = 0; i < 20; i++){
+            ingredientItem[i].textContent = selectedMeal[`strMeasure` + (i + 1) ] + " " + selectedMeal[`strIngredient` + (i + 1)];
         }
-    }
+
+        for(var i = 0; i < instructionsArray.length; i++){
+            var instructionListItem = document.createElement('li');
+            instructionListItem.textContent = instructionsArray[i];
+            instructionListItem.classList.add('text-xl');
+            instructions.appendChild(instructionListItem);
+        }
+
     })
 }
-
+ 
     recipeDisplay();
