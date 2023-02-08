35da853e5b7b781195d7a222fb1da06eddf2ca05
Chris <carechiga43@gmail.com>
2023-02-07 18:28:14 -0800

    removed tailwind CLI, using CDN link instead

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
+var radnomMealCard = document.querySelectorAll(".random-meal")
+var randomMealImg = document.querySelectorAll(".random-meal-img")
+var randomMealTitle = document.querySelectorAll(".random-meal-name")
+
+
 function randomMealDisplay(){
     fetch("https://www.themealdb.com/api/json/v2/9973533/randomselection.php")
 .then(function(response){
@@ -5,6 +10,10 @@ function randomMealDisplay(){
     .then(function(data){
         console.log(data);
        
+        for(var i = 0; i < 3; i++){
+            randomMealImg[i].src = data.meals[i].strMealThumb;
+            randomMealTitle[i].textContent = data.meals[i].strMeal;
+        }
    });
 }
 
